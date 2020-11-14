e4b3844547d6e44933699060c98a6678c7fc986a
= <=>
2020-11-14 17:20:55 +0530

    Added surfacxe

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    surface = new Ground(120,350,200,160);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    surface.display();
 }
